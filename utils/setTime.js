42e5658e10fee50774766377cbd953ae1cda6b0f
ItChinaW <13245505@qq.com>
2022-06-08 20:03:11 +0800

    --fix 语音bug

--- utils/setTime.js
+++ utils/setTime.js
@@ -1,22 +1,19 @@
 const { bot } = require("../events/global");
 const { cqcode } = require("oicq");
+const moment = require("moment");
 
 // 定时
 setInterval(async () => {
-    let hours = new Date().getHours()
-    let minutes = new Date().getMinutes()
-    let hi;
-    if (hours == 12 && minutes == 00) {
+    let time = moment().format("HH:mm:ss")
+    if (time === "12:00:00") {
         hi = cqcode.record("./static/audio/M}4PW}QVADVOY@TSSN%9JF3.amr")
-    } else if (hours == 15 && minutes == 00) {
+        return
+    }
+    if (time === "15:00:00") {
         hi = cqcode.record("./static/audio/BROV~`Y{WUPHRY`QM8SGNZ0.amr")
-    } else if (hours == 23 && minutes == 00) {
+        return
+    }
+    if (time === "23:00:00") {
         hi = cqcode.record("./static/audio/7D[2D5A6E5EI_IU@X(8S6@7.amr")
     }
-    if (hi) {
-        bot.gl.forEach((info, groupId, map) => {
-            bot.sendGroupMsg(groupId, hi)
-        })
-        hi = ""
-    }
-}, 60000);
+}, 1000);
